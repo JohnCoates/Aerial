39323d99ad54f9b87008266ca553e4d358b9af1e
John Coates <acapulco1988@gmail.com>
2015-10-22 22:25:54 -0700

    Initial Commit

---
+++ Aerial/AerialView.h
@@ -0,0 +1,13 @@
+//
+//  AerialView.h
+//  Aerial
+//
+//  Created by John Coates on 10/22/15.
+//  Copyright © 2015 John Coates. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface AerialView : ScreenSaverView
+
+@end
---
+++ Aerial/AerialView.m
@@ -0,0 +1,52 @@
+//
+//  AerialView.m
+//  Aerial
+//
+//  Created by John Coates on 10/22/15.
+//  Copyright © 2015 John Coates. All rights reserved.
+//
+
+#import "AerialView.h"
+
+@implementation AerialView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
