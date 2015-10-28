bd2c6c5f3e573a86c4a4bd0f5e89bf0aa328f26e
John Coates <acapulco1988@gmail.com>
2015-10-28 14:36:35 -0700

    added CollectionType+Shuffling

---
+++ Aerial/Source/Models/Extensions/CollectionType+Shuffling.swift
@@ -0,0 +1,32 @@
+//
+//  CollectionType+Shuffling.swift
+//  Aerial
+//
+
+
+import Foundation
+
+// shuffling thanks to Nate Cook http://stackoverflow.com/questions/24026510/how-do-i-shuffle-an-array-in-swift
+
+extension CollectionType {
+    /// Return a copy of `self` with its elements shuffled
+    func shuffle() -> [Generator.Element] {
+        var list = Array(self)
+        list.shuffleInPlace()
+        return list
+    }
+}
+
+extension MutableCollectionType where Index == Int {
+    /// Shuffle the elements of `self` in-place.
+    mutating func shuffleInPlace() {
+        // empty and single-element collections don't shuffle
+        if count < 2 { return }
+        
+        for i in 0..<count - 1 {
+            let j = Int(arc4random_uniform(UInt32(count - i))) + i
+            guard i != j else { continue }
+            swap(&self[i], &self[j])
+        }
+    }
+}
