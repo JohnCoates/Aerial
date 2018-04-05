0bf97aba0e2108e84df6ed263f69b8c7fe167032
Klaas <klaas@users.noreply.github.com>
2018-04-05 14:45:57 +0200

    Removed Swift 4.1 warning

--- Aerial/Source/Models/Extensions/CollectionType+Shuffling.swift
+++ Aerial/Source/Models/Extensions/CollectionType+Shuffling.swift
@@ -14,7 +14,7 @@ extension MutableCollection {
         guard c > 1 else { return }
         
         for (unshuffledCount, firstUnshuffled) in zip(stride(from: c, to: 1, by: -1), indices) {
-            let d: IndexDistance = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            let d: Int = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
             guard d != 0 else { continue }
             let i = index(firstUnshuffled, offsetBy: d)
 			self.swapAt(firstUnshuffled, i)
